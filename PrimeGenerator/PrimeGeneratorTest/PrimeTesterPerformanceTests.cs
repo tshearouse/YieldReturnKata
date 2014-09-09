302fea0c6c9f59890f76884abad3f45530f05e32
Tim Shearouse <ShearouseTimothyW@JohnDeere.com>
2014-09-09 12:07:00 -0500

    Added performance test

---
+++ PrimeGenerator/PrimeGeneratorTest/PrimeTesterPerformanceTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using NUnit.Framework;
+using PrimeGenerator;
+
+namespace PrimeGeneratorTest
+{
+    [TestFixture]
+    public class PrimeTesterPerformanceTests
+    {
+        readonly PrimeTester _primeTester = new PrimeTester();
+
+        [Test]
+        public void Given1ThroughOneHundredThousandWhenGetPrimesThenShowPerformance()
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            var primes = _primeTester.GetPrimes(1, 100000);
+            foreach (var prime in primes)
+            {
+                timer.Stop();
+                Console.WriteLine("Time: {0} Number: {1}", timer.ElapsedMilliseconds, prime);
+                timer.Reset();
+                timer.Start();
+            }
+        }
+    }
+}
