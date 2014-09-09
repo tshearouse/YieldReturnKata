5508bb6930c23061f19bf381db744071a2b29261
Tim Shearouse <ShearouseTimothyW@JohnDeere.com>
2014-09-09 11:28:23 -0500

    Initial commit with naive prime number generator.

---
+++ PrimeGenerator/PrimeGenerator/PrimeTester.cs
@@ -0,0 +1,18 @@
+﻿namespace PrimeGenerator
+{
+    public class PrimeTester
+    {
+        public bool IsPrime(int number)
+        {
+            if (number < 2) return false;
+
+            bool numberIsPrime = true;
+            for (int i = 2; i < number; ++i)
+            {
+                if (number % i == 0)
+                    numberIsPrime = false;
+            }
+            return numberIsPrime;
+        }
+    }
+}
---
+++ PrimeGenerator/PrimeGeneratorTest/PrimeTesterTest.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using PrimeGenerator;
+
+namespace PrimeGeneratorTest
+{
+    [TestFixture]
+    public class PrimeTesterTest
+    {
+        PrimeTester _primeTester = new PrimeTester();
+
+        [Test]
+        public void Given0WhenIsPrimeShouldReturnFalse()
+        {
+            bool isPrime = _primeTester.IsPrime(0);
+            Assert.IsFalse(isPrime);
+        }
+
+        [Test]
+        public void Given1WhenIsPrimeShouldReturnTrue()
+        {
+            bool isPrime = _primeTester.IsPrime(1);
+            Assert.IsFalse(isPrime);
+        }
+
+        [Test]
+        public void Given2WhenIsPrimeShouldReturnTrue()
+        {
+            bool isPrime = _primeTester.IsPrime(2);
+            Assert.IsTrue(isPrime);
+        }
+
+        [Test]
+        public void Given4WhenIsPrimeShouldReturnFalse()
+        {
+            bool isPrime = _primeTester.IsPrime(4);
+            Assert.IsFalse(isPrime);
+        }
+
+        [Test]
+        public void Given911WhenIsPrimeShouldReturnTrue()
+        {
+            bool isPrime = _primeTester.IsPrime(911);
+            Assert.IsTrue(isPrime);
+        }
+    }
+}
